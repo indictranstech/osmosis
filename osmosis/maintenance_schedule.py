35af601f52a3dd89b3aeb554acca0cc235b60d48
sagar30051991 <sagarshiragawakar@gmail.com>
2016-05-14 15:16:04 +0530

    Added the field in delivery Note,changes in maintenance_schedule,list view added in time log

--- osmosis/maintenance_schedule.py
+++ osmosis/maintenance_schedule.py
@@ -6,11 +6,10 @@ from frappe.utils import add_days, getdate, cint, cstr
 from frappe import throw, _
 from frappe.utils import add_days, getdate, formatdate, get_first_day, date_diff, today, add_years
 
-
 def auto_status_update_ms(doc, method):
 	new = add_years(doc.installation_date, doc.guarantee_period)
-	
-	doc.amc_guarantee_valid_upto_date=new
+	validdate_amc1=add_days(new,-1)
+	doc.amc_guarantee_valid_upto_date=validdate_amc1
 	if doc.transaction_date:
 		doc.amc_status="Untraceable"
 	if date_diff(doc.transaction_date,doc.installation_date)<=365*doc.contract_period:
@@ -21,5 +20,6 @@ def auto_status_update_ms(doc, method):
 		doc.amc_status="Expired"
 	if doc.amc_start_month:
 		guarntee=add_years(doc.amc_start_month,doc.contract_period)
-		doc.amc_guarantee_valid_upto_date=guarntee
+		validdate_amc=add_days(guarntee,-1)
+		doc.amc_guarantee_valid_upto_date=validdate_amc
 		doc.amc_status = "AMC"
