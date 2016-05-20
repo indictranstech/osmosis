17cdf301ef7f1222fd6901381779e6655ea12498
sagar30051991 <sagarshiragawakar@gmail.com>
2016-05-20 18:28:10 +0530

    added the import option for MS & added the Flat No field in contact

--- osmosis/maintenance_schedule.js
+++ osmosis/maintenance_schedule.js
@@ -1,8 +1,7 @@
 cur_frm.add_fetch('Sales_Order', 'advance_paid', 'pay_recevied');
 cur_frm.add_fetch('Sales_Order', 'client_id', 'client_id');
-cur_frm.add_fetch('Customer', 'client_id', 'client_id');
 
-frappe.ui.form.on("Maintenance Schedule", {
+/*frappe.ui.form.on("Maintenance Schedule", {
 	refresh: function(frm) {
 		cur_frm.fields_dict.items.grid.get_field("wing").get_query = function(doc) {
 				return {filters: {
@@ -22,4 +21,4 @@ frappe.ui.form.on("Maintenance Visit", {
 				}
 		}
 	}
-})
+})*/
